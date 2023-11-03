9d84c6cfadb51862c8c4e22b4d82d4f3cbb13675
Marco Xu <marco79811@hotmail.com>
2023-11-03 15:57:47 +0800

    pull from GH

--- src/main/java/com/pichill/back/Test.java
+++
@@ -1,7 +0,0 @@
-package com.pichill.back;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("hi");
-	}
-}
--- src/main/java/com/pichill/generaluser/GeneralUser.java
+++
@@ -1,7 +0,0 @@
-package com.pichill.generaluser;
-
-public class GeneralUser {
-	private Integer genId;
-	private String genAcc;
-	private String genPassword;
-}
