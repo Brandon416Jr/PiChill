0fd83fd8105ec37a6cb940a520b6b0ed82ed9222
Marco Xu <marco79811@hotmail.com>
2023-11-17 17:06:28 +0800

    create new file and modified file name

---
+++ src/main/java/com/pichill/coupon/TestCoupon.java
@@ -0,0 +1,21 @@
+package com.pichill.coupon;
+
+import java.util.Scanner;
+
+public class TestCoupon {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("輸入要新增的編號");
+		int couponID = sc.nextInt();
+		System.out.println("輸入要新增的部門名");
+		int productID = sc.nextInt();
+
+
+		sc.close();
+
+	
+		System.out.println("=====================");
+	}
+
+}
