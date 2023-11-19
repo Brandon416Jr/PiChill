bc61cd6fb49adf3d96a52263d3e07799810ab361
Marco Xu <marco79811@hotmail.com>
2023-11-19 17:21:53 +0800

    create new files

--- src/main/java/com/pichill/coupon/Coupon.java
+++ src/main/java/com/pichill/coupon/Coupon.java
@@ -1,5 +1,27 @@
 package com.pichill.coupon;
 
-public class Coupon {
+import java.io.Serializable;
 
+@SuppressWarnings("serial")
+public class CouponVO implements Serializable {
+	private Integer couponID;
+	private Integer productID;
+	public Integer getCouponID() {
+		return couponID;
+	}
+	public void setCouponID(Integer couponID) {
+		this.couponID = couponID;
+	}
+	public Integer getProductID() {
+		return productID;
+	}
+	public void setProductID(Integer productID) {
+		this.productID = productID;
+	}
+	@Override
+	public String toString() {
+		return "CouponVO [couponID=" + couponID + ", productID=" + productID + "]";
+	}
+	
+	
 }
