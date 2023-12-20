02b6d103c903b68fb514da245cc918eed84f5e45
Hsiao <a86020188@yahoo.com>
2023-12-20 20:49:26 +0800

    join

--- src/main/java/com/pichill/forumlike/entity/ForumLike.java
+++ src/main/java/com/pichill/forumlike/entity/ForumLike.java
@@ -28,66 +28,45 @@ public class ForumLike {
 //	@Column(name = "gUserID")
 //	private Integer gUserID;
 	
-//	@ManyToOne
-//	@JoinColumn(name = "postID",referencedColumnName = "postID")
-//	private Post post;
-	private Integer postID;
+	@ManyToOne
+	@JoinColumn(name = "postID",referencedColumnName = "postID")
+	private Post post;
+//	private Integer postID;
 	
 	@Column(name = "likeStatus")
 	private boolean likeStatus;
-	public ForumLike() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public ForumLike(Integer likeID, Integer gUserID, Integer postID) {
-		super();
-		this.likeID = likeID;
-//		this.gUserID = gUserID;
-//		this.postID = postID;
-	}
+
 	public Integer getLikeID() {
 		return likeID;
 	}
+
 	public void setLikeID(Integer likeID) {
 		this.likeID = likeID;
 	}
-//	public Integer getgUserID() {
-//		return gUserID;
-//	}
-//	public void setgUserID(Integer gUserID) {
-//		this.gUserID = gUserID;
-//	}
-	
-	public Integer getPostID() {
-		return postID;
-	}
+
 	public GeneralUser getGeneralUser() {
 		return generalUser;
 	}
+
 	public void setGeneralUser(GeneralUser generalUser) {
 		this.generalUser = generalUser;
 	}
-	public void setPostID(Integer postID) {
-		this.postID = postID;
+
+	public Post getPost() {
+		return post;
 	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
 	public boolean isLikeStatus() {
 		return likeStatus;
 	}
+
 	public void setLikeStatus(boolean likeStatus) {
 		this.likeStatus = likeStatus;
 	}
-	@Override
-	public String toString() {
-		return "ForumLike [likeID=" + likeID + ", generalUser=" + generalUser + ", postID=" + postID + ", likeStatus="
-				+ likeStatus + "]";
-	}
-	
-	
-//	public Post getPost() {
-//		return post;
-//	}
-//	public void setPost(Post post) {
-//		this.post = post;
-//	}
+
 	
 }
